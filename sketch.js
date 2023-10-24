31536ab797fd2c04d593e3127bbb326c23b59d87
Osvaldo Cobacho <oobacho@countigo.cu>
2023-10-24 13:58:52 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(400, 400);
+    // fill(0);
+    
+}
+
+
+function draw(){
+    background(0);
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text("hello world", 200, 200);
+}
