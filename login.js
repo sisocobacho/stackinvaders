f7a9ad5fc678e026d8ab2945a63111cd3bcde537
Osvaldo Cobacho <oobacho@countigo.cu>
2023-10-28 01:43:48 -0400

    login skevm

---
+++ login.js
@@ -0,0 +1,24 @@
+
+
+const passport = new window.immutable.passport.Passport({
+  baseConfig: new window.immutable.config.ImmutableConfiguration({
+    environment: window.immutable.config.Environment.SANDBOX,
+  }),
+  clientId: 'e6lS9dxoPiDfNRu33C1PslqpTnVYcVJw',
+  redirectUri: 'https://stackinvaders.vercel.app/',
+  logoutRedirectUri: 'https://stackinvaders.vercel.app/#logout',
+  audience: 'platform_api',
+  scope: 'openid offline_access email transact'
+});
+
+const provider = passport.connectEvm();
+const accounts = await provider.request({ method: "eth_requestAccounts" });
+window.addEventListener('load', function() {
+    passport.loginCallback();
+});
+
+// passport.logout();
+
+const userProfile = await passport.getUserInfo();
+
+console.log("userrrrrr", userProfile);
