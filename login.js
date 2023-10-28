5a31e1f2e35ca8fbb389662bc5f128fdc5236b9f
Osvaldo Cobacho <oobacho@countigo.cu>
2023-10-28 14:51:15 -0400

    separating passport and login scripts

--- login.js
+++ login.js
@@ -1,22 +1,10 @@
-window.passport = new window.immutable.passport.Passport({
-  baseConfig: new window.immutable.config.ImmutableConfiguration({
-    environment: window.immutable.config.Environment.SANDBOX,
-  }),
-  clientId: 'e6lS9dxoPiDfNRu33C1PslqpTnVYcVJw',
-  redirectUri: 'https://stackinvaders.vercel.app/game.html',
-  logoutRedirectUri: 'https://stackinvaders.vercel.app/logout.html',
-  audience: 'platform_api',
-  scope: 'openid offline_access email transact'
-});
 
 window.provider = window.passport.connectEvm();
 window.accounts = await window.provider.request({ method: "eth_requestAccounts" });
-// window.addEventListener('load', function() {
-//     passport.loginCallback();
-// });
+window.addEventListener('load', function() {
+    window.passport.loginCallback();
+});
 
 // passport.logout();
 
-window.userProfile = await passport.getUserInfo();
 
-console.log("userrrrrr", userProfile);
