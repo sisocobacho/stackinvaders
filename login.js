4390a382ecb22c885f409c484c0ec0bdf98f9a63
Osvaldo Cobacho <oobacho@countigo.cu>
2023-10-28 10:00:00 -0400

    login and game separate files

--- login.js
+++ login.js
@@ -1,12 +1,10 @@
-
-
 const passport = new window.immutable.passport.Passport({
   baseConfig: new window.immutable.config.ImmutableConfiguration({
     environment: window.immutable.config.Environment.SANDBOX,
   }),
   clientId: 'e6lS9dxoPiDfNRu33C1PslqpTnVYcVJw',
-  redirectUri: 'https://stackinvaders.vercel.app/',
-  logoutRedirectUri: 'https://stackinvaders.vercel.app/#logout',
+  redirectUri: 'https://stackinvaders.vercel.app/game.html',
+  logoutRedirectUri: 'https://stackinvaders.vercel.app/logout.html',
   audience: 'platform_api',
   scope: 'openid offline_access email transact'
 });
